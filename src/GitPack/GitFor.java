2d2a6b3f98c7e452b0706a1349a5500c52207c75
Alexey <AlexNov8344@yandex.ru>
2022-02-06 10:17:22 +0300

    Class Moloko add

--- src/GitPack/GitFor.java
+++ src/GitPack/GitFor.java
@@ -2,6 +2,8 @@ package GitPack;
 
 public class GitFor {
     public static void main(String[] args) {
-
+        Moloko milk = new Moloko ();
+        System.out.println(milk);
     }
 }
+
---
+++ src/GitPack/Moloko.java
@@ -0,0 +1,4 @@
+package GitPack;
+
+public class Moloko {
+}
